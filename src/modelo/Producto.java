856efac5d58b2976df90426959686c19e19beea8
vina <vina@VIAWSE02LC0509.duoc.acad>
2025-11-12 12:51:57 -0300

    "avance en clases? al inicio"

--- src/modelo/Producto.java
+++ src/modelo/Producto.java
@@ -13,6 +13,53 @@ public class Producto {
     private String nombre;
     private int stock;
     private double precio;
+
+    public Producto() {
+    }
+
+    public Producto(int id, String nombre, int stock, double precio) {
+        this.id = id;
+        this.nombre = nombre;
+        this.stock = stock;
+        this.precio = precio;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getStock() {
+        return stock;
+    }
+
+    public void setStock(int stock) {
+        this.stock = stock;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
+
+    @Override
+    public String toString() {
+        return "Producto{" + "id=" + id + ", nombre=" + nombre + ", stock=" + stock + ", precio=" + precio + '}';
+    }
     
     
 }
